89d583c26ff124e53b554da2d6ed665a9675d1a1
Max.K <max.kostin.dev@gmail.com>
2024-08-20 14:14:37 +0300

    build: 0.1.2

--- example/src/screens/Home/screen.tsx
+++ example/src/screens/Home/screen.tsx
@@ -1,11 +1,15 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import { useStyles } from './screen.styles';
+import { ThemeManager } from '../../styles';
+
+const { useTheme } = ThemeManager;
 
 export const HomeScreen = () => {
     const styles = useStyles({ overrideAutoScale: false, overrideThemeName: 'light' });
+    const theme = useTheme();
 
     return (
-        <View style={[styles.container]}>
+        <View style={[styles.container, { backgroundColor: theme.colors.accent }]}>
             <Text style={styles.text}>Home View</Text>
             <TouchableOpacity style={styles.button} />
         </View>
