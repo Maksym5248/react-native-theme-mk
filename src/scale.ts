246b9296985a8a09be60d31bb71e17f2b77d3c21
Vasil Kostin <vasyl.kostin@flip.shop>
2024-09-24 17:37:01 +0200

    feat: use only symmetric scale

--- src/scale.ts
+++ src/scale.ts
@@ -1,5 +1,5 @@
 import { PixelRatio, type StyleSheet } from 'react-native';
-import type { IDimensionDevice, IScale } from './types';
+import type { IScale } from './types';
 
 const SCALABLE_PROPS = new Set([
     'width',
@@ -46,75 +46,13 @@ const SCALABLE_PROPS = new Set([
     'translateY',
 ]);
 
-const HORIZONTAL_SCALABLE_PROPS = new Set([
-    'paddingHorizontal',
-    'paddingLeft',
-    'paddingRight',
-    'marginHorizontal',
-    'marginLeft',
-    'marginRight',
-    'width',
-    'minWidth',
-    'maxWidth',
-    'left',
-    'right',
-    'translateX',
-]);
-const VERTICAL_SCALABLE_PROPS = new Set([
-    'marginVertical',
-    'marginTop',
-    'marginBottom',
-    'paddingVertical',
-    'paddingTop',
-    'paddingBottom',
-    'height',
-    'minHeight',
-    'maxHeight',
-    'top',
-    'bottom',
-    'translateY',
-]);
-const MAX_WIDTH = new Set(['width', 'minWidth', 'maxWidth']);
-
-export const applyScaleToValue = (value: number, key: string, scale: IScale, dimensionDevice: IDimensionDevice) => {
-    if (HORIZONTAL_SCALABLE_PROPS.has(key)) {
-        if (MAX_WIDTH.has(key)) {
-            const newValue = value * scale.scaleW;
-            const scaledValue = newValue > dimensionDevice.width ? dimensionDevice.width : newValue;
-
-            return {
-                [key]: PixelRatio.roundToNearestPixel(scaledValue),
-            };
-        }
-        return {
-            [key]: PixelRatio.roundToNearestPixel(value * scale.scaleW),
-        };
-    } else if (VERTICAL_SCALABLE_PROPS.has(key)) {
-        return {
-            [key]: PixelRatio.roundToNearestPixel(value * scale.scaleH),
-        };
-    } else if (key === 'margin') {
-        return {
-            ['marginVertical']: PixelRatio.roundToNearestPixel(value * scale.scaleH),
-            ['marginHorizontal']: PixelRatio.roundToNearestPixel(value * scale.scaleW),
-        };
-    } else if (key === 'padding') {
-        return {
-            ['paddingVertical']: PixelRatio.roundToNearestPixel(value * scale.scaleH),
-            ['paddingHorizontal']: PixelRatio.roundToNearestPixel(value * scale.scaleW),
-        };
-    } else {
-        return {
-            [key]: PixelRatio.roundToNearestPixel(value * scale.scaleFactor),
-        };
-    }
+export const applyScaleToValue = (value: number, key: string, scale: IScale) => {
+    return {
+        [key]: PixelRatio.roundToNearestPixel(value * scale.symmetric),
+    };
 };
 
-export function applyScale<B extends StyleSheet.NamedStyles<B> | StyleSheet.NamedStyles<any>>(
-    styles: B,
-    scale: IScale,
-    dimensionDevice: IDimensionDevice,
-): B {
+export function applyScale<B extends StyleSheet.NamedStyles<B> | StyleSheet.NamedStyles<any>>(styles: B, scale: IScale): B {
     const scaledStyles = {} as B;
 
     for (const key in styles) {
@@ -122,11 +60,10 @@ export function applyScale<B extends StyleSheet.NamedStyles<B> | StyleSheet.Name
             const value = styles[key as keyof B];
 
             if (typeof value === 'object' && value !== null) {
-                scaledStyles[key as keyof B] = applyScale(value as StyleSheet.NamedStyles<B>, scale, dimensionDevice) as B[keyof B];
+                scaledStyles[key as keyof B] = applyScale(value as StyleSheet.NamedStyles<B>, scale) as B[keyof B];
             } else if (typeof value === 'number' && SCALABLE_PROPS.has(key)) {
-                const newScaledStyle = applyScaleToValue(value, key, scale, dimensionDevice);
-
-                Object.assign(scaledStyles, newScaledStyle);
+                const modifiedValue = PixelRatio.roundToNearestPixel(value * scale.symmetric);
+                scaledStyles[key as keyof B] = modifiedValue as StyleSheet.NamedStyles<B> as B[keyof B];
             } else {
                 scaledStyles[key as keyof B] = value;
             }
